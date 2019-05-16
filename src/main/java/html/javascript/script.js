c3b9c7b3a33f7fbbd0de109014fe14fc81d0c802
Mircea <bodea.mircea@gmail.com>
2019-05-16 09:27:20 +0300

    Added a login screen to the project, it's just visual with html and css, will be connected to the DB later on, cool stuff learned from youtube, link: https://www.youtube.com/watch?v=HV7DtH3J2PU , also important to deploy the servlet :D

---
+++ src/main/java/html/javascript/script.js
@@ -0,0 +1,14 @@
+
+var add = function(number1, number2){
+    return number1+number2;
+}
+$(document).ready(function () {
+var tabel = $(".content_class");
+tabel.click(function () {
+    alert("You clicked a paragraph!");
+});
+});
+
+$(document).ready(function(){
+    var meniu_lateral=$("#sideMenu")
+})
